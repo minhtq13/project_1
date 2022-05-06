f7bd86c6bc28d5d02b65764f6fae5677606daa79
minhtq13 <taminh596@gmail.com>
2022-05-06 23:21:11 +0700

    Clean Architeture

---
+++ src/components/Task/Task.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./Task.scss";
+
+function Task() {
+    return (
+        <li className="task-item">
+            <img
+                src="https://znews-photo.zingcdn.me/w360/Uploaded/natmzz/2022_05_05/me.JPG"
+                alt="project_1-img"
+            />
+            Title
+        </li>
+    );
+}
+
+export default Task;
